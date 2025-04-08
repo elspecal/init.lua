91a795d3cb8bf3ec821477800f9d23aa7ced7166
Miklos Csepella <miklos.csepella@goto.com>
2025-04-08 10:58:40 +0200

    use a command alias instead of a user command to map "G" to Neogit in command mode

--- init.lua
+++ init.lua
@@ -3,7 +3,6 @@ require("miki.lsp")
 require("miki.set")
 require("miki.map")
 require("miki.autocmd")
-require("miki.usercmd")
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
--- lua/miki/map.lua
+++ lua/miki/map.lua
@@ -1,2 +1,4 @@
 vim.keymap.set({ "i", "v" }, "kj", "<Esc>", { silent = true, noremap = true })
 vim.keymap.set("n", "<leader>e", function () MiniFiles.open() end, { noremap = true })
+
+vim.cmd("cnoreabbrev G Neogit")
