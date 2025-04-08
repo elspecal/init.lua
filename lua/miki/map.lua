e05ac09d5d007075536a1d26419020cdea6a65ac
Miklos Csepella <miklos.csepella@goto.com>
2025-04-08 12:42:23 +0200

    add keymaps for yanking to "+" register (global clipboard)

--- lua/miki/map.lua
+++ lua/miki/map.lua
@@ -1,4 +1,6 @@
 vim.keymap.set({ "i", "v" }, "kj", "<Esc>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>e", function () MiniFiles.open() end, { noremap = true })
+vim.keymap.set("n", "<leader>e", function() MiniFiles.open() end, { noremap = true })
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>Y", [["+Y]])
 
 vim.cmd("cnoreabbrev G Neogit")
